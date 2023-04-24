8a276483ad3834338c219a08c5e37f97e16910b8
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-04-24 15:52:32 +0200

    Test styles

---
+++ script.js
@@ -0,0 +1,7 @@
+let h2s = document.getElementsByTagName('h2');
+
+for (let h2 of h2s) {
+  h2.classList.add('uppercase', 'font-bold', 'text-xl');
+}
+
+let navElements = document.getElementsByTagName('nav');
