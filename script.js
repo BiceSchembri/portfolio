c553ce5dd2c56f762e9df6f0f77705eabe6cc889
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-04-25 16:30:39 +0200

    Add back to top button

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-let h2s = document.getElementsByTagName('h2');
+let backToTopButton = document.getElementById('backToTopButton');
 
-for (let h2 of h2s) {
-  h2.classList.add('uppercase', 'font-bold', 'text-xl');
+// When the user scrolls down 20px from the top of the document, show the button
+onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
 }
 
-let navElements = document.getElementsByTagName('nav');
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
