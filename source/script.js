507a97cedef4003278a2d4d024ec39c6582562f5
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-04-26 16:17:38 +0200

    Add photo carousel

--- source/script.js
+++ source/script.js
@@ -1,6 +1,7 @@
+// Back to top button (appears when the user scrolls down)
+
 let backToTopButton = document.getElementById('backToTopButton');
 
-// When the user scrolls down 20px from the top of the document, show the button
 onscroll = function () {
   scrollFunction();
 };
@@ -13,8 +14,36 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Picture slideshow on Projects cards
+
+let slideIndex = [1, 1, 1, 1];
+let slideId = ['slides1', 'slides2', 'slides3', 'slides4'];
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+
+function plusSlides(n, no) {
+  showSlides((slideIndex[no] += n), no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  console.log(x);
+  if (n > x.length) {
+    slideIndex[no] = 1;
+  }
+  if (n < 1) {
+    slideIndex[no] = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  x[slideIndex[no] - 1].style.display = 'block';
 }
